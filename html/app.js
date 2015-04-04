f23f4c07c69f70a1a27d984cee909ae44176a6b9
Roshan Thapaliya <roshan.thapaliya@bison.howard.edu>
2015-04-04 15:55:50 -0400

    form added

--- html/app.js
+++ html/app.js
@@ -13,3 +13,20 @@ document.addEventListener('polymer-ready', function() {
     drawerPanel.togglePanel();
   });
 });
+
+
+$(document).ready(function(){
+        $("#addform").click(function () {
+            $("#content").fadeIn(1 );
+        });
+
+    });
+
+function validate() {
+            var decorator = document.getElementById('paper1');
+            var input = document.getElementById('input1');
+            decorator.isInvalid = !input.validity.valid;
+        }
+
+
+
